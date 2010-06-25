f7fff6013e266f081d23e48e5c1ad6bd107184da
Nick Payne <nick@kurai.co.uk>
2010-06-25 20:58:12 +0100

    session class now exists, along with some unit tests too!

---
+++ library/tests/session_test.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+if (!defined("BASE")) {
+	define("BASE", __DIR__."/../");
+}
+
+require(BASE."session.php");
+
+class SessionTest extends PHPUnit_Framework_TestCase {
+    private $session = NULL;
+
+    protected function setUp() {
+        $this->session = Session::getInstance("foo_bar_baz_xa12gf");
+    }
+
+    protected function tearDown() {
+        $this->session->destroy();
+    }
+
+    public function testSetAndGetWithStrings() {
+        $this->session->foo = "bar";
+        $this->assertEquals("bar", $this->session->foo);
+    }
+
+    public function testGetWithInvalidVariable() {
+        $this->assertNull($this->session->foo);
+    }
+
+    public function testSetOverridesAlreadySetVariable() {
+        $this->session->foo = "bar";
+        $this->assertEquals("bar", $this->session->foo);
+        $this->session->foo = "baz";
+        $this->assertEquals("baz", $this->session->foo);
+    }
+
+    public function testSetAndGetWithArrays() {
+        $this->session->foo = array(10, 20, 30);
+        $this->assertEquals(array(10, 20, 30), $this->session->foo);
+    }
+
+    public function testSetAndGetWithObjects() {
+        $o = new stdClass();
+        $o->var = "val";
+        $o->foo = "bar";
+
+        $this->session->foo = $o;
+
+        $this->assertEquals($o, $this->session->foo);
+    }
+}
