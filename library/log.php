8addc9bb69100c88b8bdd50bd8013081a62ea965
Nick Payne <nick@kurai.co.uk>
2010-10-06 22:04:43 +0100

    removing php 5.3 dependency

--- library/log.php
+++ library/log.php
@@ -4,11 +4,11 @@ class Log {
 	public static function debug($str, $verbose=NULL) {
 		// determine log mode...
 		// write log
-		static::_log($str, "debug");
+		self::_log($str, "debug");
 	}
 	
 	private static function _log($str, $level) {
-		if (static::$handle === NULL) {
+		if (self::$handle === NULL) {
             $path = Settings::getValue("log.".$level);
             if (!file_exists($path)) {
                 throw new CoreException("Logfile does not exist", CoreException::LOG_FILE_ERROR, array("path" => $path));
@@ -16,11 +16,11 @@ class Log {
             if (!is_writable($path)) {
                 throw new CoreException("Logfile is not writable", CoreException::LOG_FILE_ERROR, array("path" => $path));
             }
-			static::$handle = fopen($path, "a");
-			if (!static::$handle) {
+			self::$handle = fopen($path, "a");
+			if (!self::$handle) {
 				throw new CoreException("Could not open logfile for writing", CoreException::LOG_FILE_ERROR, array("path" => $path));
 			}
 		}
-		fwrite(static::$handle, date("d/m/Y H:i:s")." - ".$str.PHP_EOL);
+		fwrite(self::$handle, date("d/m/Y H:i:s")." - ".$str.PHP_EOL);
 	}
 }
