0a14f5319890ffe2c85583291fbbe7b559d8f4f2
Nick Payne <nick@kurai.co.uk>
2010-12-29 19:21:28 +0000

    fixing classname typo, adding more helpful test messages

--- tests/library/ErrorHandlerTest.php
+++ tests/library/ErrorHandlerTest.php
@@ -1,5 +1,5 @@
 <?php
-class ErrorHandlerrTest extends PHPUnit_Framework_TestCase {
+class ErrorHandlerTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->eh = new ErrorHandler();
     }
@@ -11,7 +11,7 @@ class ErrorHandlerrTest extends PHPUnit_Framework_TestCase {
     public function testErrorCodeZeroWithVerboseErrors() {
         $e = new CoreException("Some Default Exception");
         $this->eh->handleError($e);
-        $this->assertEquals(404, $this->eh->getResponse()->getResponseCode());
-        $this->assertTrue(strpos($this->eh->getResponse()->getBody(), "Some Default Exception") !== false);
+        $this->assertEquals(404, $this->eh->getResponse()->getResponseCode(), "Correct response code");
+        $this->assertTrue(strpos($this->eh->getResponse()->getBody(), "Some Default Exception") !== false, "Body has correct contents");
     }
 }
