e9f7bdc0bca28d597733e56ca943e8a0b2b87855
Nick Payne <nick@kurai.co.uk>
2011-06-17 14:03:24 +0100

    added a few file tests

--- tests/library/FileTest.php
+++ tests/library/FileTest.php
@@ -2,6 +2,14 @@
 
 class FileTest extends PHPUnit_Framework_TestCase {
 
+    protected $fileData = array(
+        "name" => "testfile.jpeg",
+        "type" => "image/jpeg",
+        "tmp_name" => "/tmp/fake12345",
+        "error" => 0,
+        "size" => 123456,
+    );
+
     public function testErrorsWithNoData() {
         $file = new File();
         $this->assertEquals(File::ERR_NO_FILE, $file->getError());
@@ -9,24 +17,31 @@ class FileTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testGetFilename() {
-        $file = new File(array(
-            "name" => "testfile.jpeg",
-            "type" => "image/jpeg",
-            "tmp_name" => "/tmp/fake12345",
-            "error" => 0,
-            "size" => 123456,
-        ));
+        $file = new File($this->fileData);
         $this->assertEquals("testfile.jpeg", $file->getFileName());
     }
 
     public function testGetFileExt() {
-        $file = new File(array(
-            "name" => "testfile.jpeg",
-            "type" => "image/jpeg",
-            "tmp_name" => "/tmp/fake12345",
-            "error" => 0,
-            "size" => 123456,
-        ));
+        $file = new File($this->fileData);
         $this->assertEquals("jpeg", $file->getFileExt());
+    }
+
+    public function testGetFileExtWithNoDotInFilename() {
+        $this->fileData['name'] = 'myFile';
+        $file = new File($this->fileData);
+        $this->assertEquals("", $file->getFileExt());
+    }
+
+    public function testGetMessage() {
+        $file = new File($this->fileData);
+        $this->assertEquals("The file was uploaded successfully", $file->getMessage());
+
+        $this->fileData['error'] = 99;
+        $file = new File($this->fileData);
+        $this->assertEquals("No file uploaded", $file->getMessage());
+
+        $this->fileData['error'] = 99999;
+        $file = new File($this->fileData);
+        $this->assertEquals("Unknown error", $file->getMessage());
     }
 }
