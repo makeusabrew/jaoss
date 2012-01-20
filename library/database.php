cce45832759e955bf1fd3e0bea75637fe8888ccd
Nick Payne <nick@kurai.co.uk>
2012-01-20 13:34:08 +0000

    neater way to support different MySQL connection charsets

--- library/database.php
+++ library/database.php
@@ -14,18 +14,17 @@ class Db extends PDO {
         if (self::$instance === NULL) {
 
             $dsn = "mysql:dbname=".Settings::getValue("db", "dbname").";host=".Settings::getValue("db", "host");
-            Log::verbose("Instantiating db dsn [".$dsn."] user [".Settings::getValue("db", "user")."]");
 
             $options = array();
-            if (Settings::getValue("db", "encoding", false) == "utf8") {
-                $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES 'UTF8'";
+            if (($charset = Settings::getValue("db", "charset", false)) != false) {
+                $dsn .=";charset=".$charset;
             }
+            Log::verbose("Instantiating db dsn [".$dsn."] user [".Settings::getValue("db", "user")."]");
 
             self::$instance = new Db(
                 $dsn,
                 Settings::getValue("db", "user"),
-                Settings::getValue("db", "pass"),
-                $options
+                Settings::getValue("db", "pass")
             );
 
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
