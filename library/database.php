6bb572a24bc57167544e6f6b90a1b42c8a3270d7
Nick Payne <nick@kurai.co.uk>
2012-01-20 13:30:04 +0000

    add optional MySQL connection mode info to support utf8 charset

--- library/database.php
+++ library/database.php
@@ -1,31 +1,37 @@
 <?php
 class Db extends PDO {
 
-	/**
-	 * @var Db Singleton instance
-	 */
-	private static $instance = NULL;
+    /**
+    * @var Db Singleton instance
+    */
+    private static $instance = NULL;
 
-	/**
-	 * Get the singleton instance
-	 * @return Db
-	 */
-	public static function getInstance() {
-		if (self::$instance === NULL) {
+    /**
+    * Get the singleton instance
+    * @return Db
+    */
+    public static function getInstance() {
+        if (self::$instance === NULL) {
 
             $dsn = "mysql:dbname=".Settings::getValue("db", "dbname").";host=".Settings::getValue("db", "host");
             Log::verbose("Instantiating db dsn [".$dsn."] user [".Settings::getValue("db", "user")."]");
-			self::$instance = new Db(
+
+            $options = array();
+            if (Settings::getValue("db", "encoding", false) == "utf8") {
+                $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES 'UTF8'";
+            }
+
+            self::$instance = new Db(
                 $dsn,
-				Settings::getValue("db", "user"),
-				Settings::getValue("db", "pass")
-			);
-			
-			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                Settings::getValue("db", "user"),
+                Settings::getValue("db", "pass"),
+                $options
+            );
 
-		}
-		
-		return self::$instance;
-	}
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        }
+
+        return self::$instance;
+    }
 }
