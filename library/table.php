34416a9818d7a0e31c99240d4fc6518b5a13e7ae
Nick Payne <nick@kurai.co.uk>
2012-02-06 20:23:21 +0000

    creating low level single query method as well as queryAll

--- library/table.php
+++ library/table.php
@@ -201,11 +201,24 @@ class Table {
 
     public function queryAll($sql, $params = array(), $objectName = NULL) {
         $objectName = $objectName ? $objectName : $this->getObjectName();
-		$dbh = Db::getInstance();
-		$sth = $dbh->prepare($sql);
-		$sth->setFetchMode(PDO::FETCH_CLASS, $objectName);
-		$sth->execute($params);
-		return $sth->fetchAll();
+        return $this->doQuery($sql, $params, $objectName, true);
+    }
+
+    public function query($sql, $params = array(), $objectName = NULL) {
+        $objectName = $objectName ? $objectName : $this->getObjectName();
+        return $this->doQuery($sql, $params, $objectName, false);
+    }
+
+    protected function doQuery($sql, $params, $objectName, $all) {
+        $dbh = Db::getInstance();
+        $sth = $dbh->prepare($sql);
+        $sth->setFetchMode(PDO::FETCH_CLASS, $objectName);
+        $sth->execute($params);
+        if ($all === true) {
+            return $sth->fetchAll();
+        } else {
+            return $sth->fetch();
+        }
     }
 		
 }
