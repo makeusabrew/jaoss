e7874d52f1c38734b9bb53895eac82fc961862b2
Nick Payne <nick@kurai.co.uk>
2012-04-25 14:46:49 +0100

    retab table.php

--- library/table.php
+++ library/table.php
@@ -1,44 +1,44 @@
 <?php
 
 class Table {
-	protected $primary_key = "id";
-	protected $parent_key = "parent_id";
-	protected $order_by = "`created` ASC";
-	
-	protected $object_name = NULL;
-	protected $table = NULL;
+    protected $primary_key = "id";
+    protected $parent_key = "parent_id";
+    protected $order_by = "`created` ASC";
+    
+    protected $object_name = NULL;
+    protected $table = NULL;
 
     protected $storeUpdated = true;
     protected $storeCreated = true;
-	
-	protected $meta = array();
-	
-	public static function getNewObject($model) {
-		$model = self::factory($model);
-		return $model->newObject();
-	}
-	
-	public function newObject() {
-		$name = $this->getObjectName();
-		return new $name;
-	}
-	
-	public static function factory($model) {
-		$m_class = $model;
-		if (class_exists($m_class)) {
-			return new $m_class;
-		}
-		$apps = AppManager::getAppPaths();
+    
+    protected $meta = array();
+    
+    public static function getNewObject($model) {
+        $model = self::factory($model);
+        return $model->newObject();
+    }
+    
+    public function newObject() {
+        $name = $this->getObjectName();
+        return new $name;
+    }
+    
+    public static function factory($model) {
+        $m_class = $model;
+        if (class_exists($m_class)) {
+            return new $m_class;
+        }
+        $apps = AppManager::getAppPaths();
         $modelFile = Utils::fromCamelCase($model).".php";
         $paths = array();
-		foreach ($apps as $app) {
-			$path = PROJECT_ROOT."apps/{$app}/models/".$modelFile;
-			if (file_exists($path)) {
-				include($path);
-				return self::factory($model);
-			}
-		}
-		throw new CoreException(
+        foreach ($apps as $app) {
+            $path = PROJECT_ROOT."apps/{$app}/models/".$modelFile;
+            if (file_exists($path)) {
+                include($path);
+                return self::factory($model);
+            }
+        }
+        throw new CoreException(
             "Could not find model in any path: ".$model,
             CoreException::MODEL_CLASS_NOT_FOUND,
             array(
@@ -47,30 +47,30 @@ class Table {
                 "file" => $modelFile,
             )
         );
-	}
-	
-	public function getObjectName() {
-		if (!isset($this->object_name)) {
-			$name = get_class($this);
-			$this->object_name = substr($name, 0, -1);
-		}
+    }
+    
+    public function getObjectName() {
+        if (!isset($this->object_name)) {
+            $name = get_class($this);
+            $this->object_name = substr($name, 0, -1);
+        }
         if (!class_exists($this->object_name)) {
             throw new CoreException("Object class does not exist: ".$this->object_name);
         }
-		return $this->object_name;
-	}
-	
-	public function getTable() {
-		if (!isset($this->table)) {
-			$table = Utils::fromCamelCase(get_class($this));
-			$this->table = "{$table}";
-		}
-		return $this->table;
-	}
+        return $this->object_name;
+    }
+    
+    public function getTable() {
+        if (!isset($this->table)) {
+            $table = Utils::fromCamelCase(get_class($this));
+            $this->table = "{$table}";
+        }
+        return $this->table;
+    }
 
     public function countAll($where = null, $params = null, $order_by = null) {
-		$q = "SELECT COUNT(*) as count FROM `".$this->getTable()."`";
-		if ($where !== NULL) {
+        $q = "SELECT COUNT(*) as count FROM `".$this->getTable()."`";
+        if ($where !== NULL) {
             if (is_array($where) && count($where) > 0) {
                 // add support for simple AND where clauses
                 $params = array();  // blat params
@@ -83,23 +83,23 @@ class Table {
             } else {
                 $q .= " WHERE {$where}";
             }
-		}
-		if ($order_by !== NULL) {
-			$q .= " ORDER BY {$order_by}";
-		} else if ($this->order_by !== NULL) {
-			$q .= " ORDER BY {$this->order_by}";
-		}
-		$dbh = Db::getInstance();
-		$sth = $dbh->prepare($q);
-		
-		$sth->execute($params);
+        }
+        if ($order_by !== NULL) {
+            $q .= " ORDER BY {$order_by}";
+        } else if ($this->order_by !== NULL) {
+            $q .= " ORDER BY {$this->order_by}";
+        }
+        $dbh = Db::getInstance();
+        $sth = $dbh->prepare($q);
+        
+        $sth->execute($params);
         $result = $sth->fetch();
         return $result['count'];
     }
-	
-	public function findAll($where = NULL, $params = NULL, $order_by = NULL, $limit = NULL) {
-		$q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."`";
-		if ($where !== NULL) {
+    
+    public function findAll($where = NULL, $params = NULL, $order_by = NULL, $limit = NULL) {
+        $q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."`";
+        if ($where !== NULL) {
             if (is_array($where) && count($where) > 0) {
                 // add support for simple AND where clauses
                 $params = array();  // blat params
@@ -112,26 +112,26 @@ class Table {
             } else {
                 $q .= " WHERE {$where}";
             }
-		}
-		if ($order_by !== NULL) {
-			$q .= " ORDER BY {$order_by}";
-		} else if ($this->order_by !== NULL) {
-			$q .= " ORDER BY {$this->order_by}";
-		}
-		if ($limit !== NULL) {
-			$q .= " LIMIT {$limit}";
-		}
-		$dbh = Db::getInstance();
-		$sth = $dbh->prepare($q);
-		$sth->setFetchMode(PDO::FETCH_CLASS, $this->getObjectName());
-		
-		$sth->execute($params);
-		return $sth->fetchAll();
-	}
+        }
+        if ($order_by !== NULL) {
+            $q .= " ORDER BY {$order_by}";
+        } else if ($this->order_by !== NULL) {
+            $q .= " ORDER BY {$this->order_by}";
+        }
+        if ($limit !== NULL) {
+            $q .= " LIMIT {$limit}";
+        }
+        $dbh = Db::getInstance();
+        $sth = $dbh->prepare($q);
+        $sth->setFetchMode(PDO::FETCH_CLASS, $this->getObjectName());
+        
+        $sth->execute($params);
+        return $sth->fetchAll();
+    }
 
     public function find($where = NULL, $params = NULL, $order_by = NULL) {
-		$q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."`";
-		if ($where !== NULL) {
+        $q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."`";
+        if ($where !== NULL) {
             if (is_array($where) && count($where) > 0) {
                 // add support for simple AND where clauses
                 $params = array();  // blat params
@@ -144,42 +144,42 @@ class Table {
             } else {
                 $q .= " WHERE {$where}";
             }
-		}
-		if ($order_by !== NULL) {
-			$q .= " ORDER BY {$order_by}";
-		} else if ($this->order_by !== NULL) {
-			$q .= " ORDER BY {$this->order_by}";
-		}
+        }
+        if ($order_by !== NULL) {
+            $q .= " ORDER BY {$order_by}";
+        } else if ($this->order_by !== NULL) {
+            $q .= " ORDER BY {$this->order_by}";
+        }
         $q .= " LIMIT 1";
 
-		$dbh = Db::getInstance();
-		$sth = $dbh->prepare($q);
-		$sth->setFetchMode(PDO::FETCH_CLASS, $this->getObjectName());
+        $dbh = Db::getInstance();
+        $sth = $dbh->prepare($q);
+        $sth->setFetchMode(PDO::FETCH_CLASS, $this->getObjectName());
 
-		$sth->execute($params);
-		return $sth->fetch();
-	}
-	
-	public function read($id = NULL) {
-		$q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."` WHERE `{$this->primary_key}` = ?";
-		$dbh = Db::getInstance();
-		$sth = $dbh->prepare($q);
+        $sth->execute($params);
+        return $sth->fetch();
+    }
+    
+    public function read($id = NULL) {
+        $q = "SELECT ".$this->getColumnString()." FROM `".$this->getTable()."` WHERE `{$this->primary_key}` = ?";
+        $dbh = Db::getInstance();
+        $sth = $dbh->prepare($q);
         $sth->setFetchMode(PDO::FETCH_CLASS, $this->getObjectName());
         $sth->execute(array($id));
-		return $sth->fetch();
-	}
-	
-	public function getColumnInfo($column) {
-		return isset($this->meta["columns"][$column]) ? $this->meta["columns"][$column] : NULL;
-	}
-	
-	public function getHasManyInfo($column) {
-		return isset($this->meta["has_many"][$column]) ? $this->meta["has_many"][$column] : NULL;
-	}
-	
-	public function getColumns() {
-		return $this->meta["columns"];
-	}
+        return $sth->fetch();
+    }
+    
+    public function getColumnInfo($column) {
+        return isset($this->meta["columns"][$column]) ? $this->meta["columns"][$column] : NULL;
+    }
+    
+    public function getHasManyInfo($column) {
+        return isset($this->meta["has_many"][$column]) ? $this->meta["has_many"][$column] : NULL;
+    }
+    
+    public function getColumns() {
+        return $this->meta["columns"];
+    }
 
     public function shouldStoreCreated() {
         return $this->storeCreated;
@@ -188,7 +188,7 @@ class Table {
     public function shouldStoreUpdated() {
         return $this->storeUpdated;
     }
-	
+    
     public function getColumnString($prefix = NULL) {
         $cols = $this->getColumns();
         $cols = array_keys($cols);
@@ -228,5 +228,5 @@ class Table {
             return $sth->fetch();
         }
     }
-		
+
 }
