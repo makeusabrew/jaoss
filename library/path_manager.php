8fc96002f9c58f11643966415170d69e50b8ce1e
Nick Payne <nick@kurai.co.uk>
2012-05-18 13:26:59 +0100

    improve debug message when a pattern matches input URL

--- library/path_manager.php
+++ library/path_manager.php
@@ -129,7 +129,7 @@ class PathManager {
             }
             if (preg_match("@{$pattern}@", $url, $matches)) {
 
-                Log::debug("matched pattern [".$pattern."] against URL [".$url."] method [apps/".$path->getApp()."/controllers/".$path->getController()."Controller::".$path->getAction()."]");
+                Log::debug($request->getMethod()." [".$url."] matches [".$pattern."] => [apps/".$path->getApp()."/controllers/".$path->getController()."Controller::".$path->getAction()."]");
 
                 $matches = self::cleanMatches($matches);
                 $path->setMatches($matches);
