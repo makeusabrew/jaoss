2801a247972c6fb9a9b11a847b056230b34f97c3
Nick Payne <nick@kurai.co.uk>
2012-05-23 08:46:36 +0100

    vastly improved DB profiling mechanism

--- library/profile_db.php
+++ library/profile_db.php
@@ -1,16 +1,72 @@
 <?php
 
 class ProfileDb extends Db {
-    protected static $queryCount = 0;
+    protected static $queries = array();
 
     public function prepare($statement, $options = array()) {
-        self::$queryCount ++;
+        $sth = new ProfileStatement(parent::prepare($statement, $options));
+        return $sth;
+    }
 
-        Log::db("Preparing query [".$statement."]");
-        return parent::prepare($statement, $options);
+    public static function addQuery($query, $duration) {
+        self::$queries[] = array(
+            "query"  => $query,
+            "exTime" => $duration,
+        );
     }
 
     public static function getQueryCount() {
-        return self::$queryCount;
+        return count(self::$queries);
     }
+
+    public static function getTotalQueryTimeMs($roundTo = 2) {
+        $time = 0;
+        foreach (self::$queries as $query) {
+            $time += $query['exTime'];
+        }
+        return round($time*1000, $roundTo);
+    }
+
+    public static function getQueries() {
+        return self::$queries;
+    }
+}
+
+class ProfileStatement {
+    protected $pdoStatement;
+
+    public function __construct(PDOStatement $statement) {
+        $this->pdoStatement = $statement;
+    }
+
+    public function execute(array $input_parameters) {
+        // time the actual method call
+        $start = microtime(true);
+        $result = $this->pdoStatement->execute($input_parameters);
+        $duration = microtime(true) - $start;
+
+        // substitute as best we can bound params for actual variables
+        $sql = $this->pdoStatement->queryString;
+        foreach ($input_parameters as $key => $param) {
+            if (is_string($key)) {
+                $keys[] = "/:".$key."/";
+            } else {
+                $keys[] = "/[?]/";
+            }
+        }
+        $query = preg_replace($keys, $input_parameters, $sql, 1);
+
+        Log::db("Execute [".$query."] => ".round($duration*1000, 3)."ms");
+
+        ProfileDb::addQuery($query, $duration);
+        return $result;
+    }
+
+    /**
+     * simple pass through proxy for all statement methods other than execute
+     */
+    public function __call($method, $args) {
+        return call_user_func_array(array($this->pdoStatement, $method), $args);
+    }
+
 }
