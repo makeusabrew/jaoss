56ccf467e6254529de517417bd7218be98ed4021
Nick Payne <nick@kurai.co.uk>
2013-01-22 15:39:16 +0000

    parse curl response headers properly

--- library/curl/response.php
+++ library/curl/response.php
@@ -2,14 +2,23 @@
 
 class CurlResponse {
     protected $info = array();
+    protected $headers;
     protected $body;
     
     public function setInfo(array $info = array()) {
         $this->info = $info;
     }
 
+    public function setHeaders($headerString) {
+        $this->headers = $this->parseHeaderString($headerString);
+    }
+
     public function setBody($body) {
         $this->body = $body;
+    }
+
+    public function getHeaders() {
+        return $this->headers;
     }
 
     public function getBody() {
@@ -23,5 +32,31 @@ class CurlResponse {
     public function getStatusClass() {
         $class = substr($this->getStatusCode(), 0, 1);
         return $class."xx";
+    }
+
+    protected function parseHeaderString($header) {
+
+        $headers = explode("\r\n", $header);
+        $final = array();
+
+        foreach ($headers as $header) {
+            if (trim($header) === "") {
+                continue;
+            }
+
+            if (substr($header, 0, 5) === "HTTP/") {
+                $final['status'] = $header;
+                continue;
+            }
+
+            if (preg_match("/^(.+?):(.+)$/", $header, $matches)) {
+                $key   = trim($matches[1]);
+                $value = trim($matches[2]);
+
+                $final[$key] = $value;
+            }
+        }
+
+        return $final;
     }
 }
